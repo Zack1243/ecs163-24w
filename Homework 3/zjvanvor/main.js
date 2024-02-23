8da44f45ec1acfa00022223f4c53872e19c46e76
Zachary Van Vorst <zjvanvor@ucdavis.edu>
2024-02-22 18:12:26 -0800

    almost...

--- Homework 3/zjvanvor/main.js
+++ Homework 3/zjvanvor/main.js
@@ -45,132 +45,162 @@ d3.csv("data/pokemon.csv").then(rawData => {
     };
 
 // Plot 1: Scatterplot of Psychic type Attack vs Special Attack
-    firstData = rawData.map(d => {
-        return {
-            "Sp_Atk": +d.Sp_Atk,
-            "Attack": +d.Attack,
-            "Type_1": d.Type_1
-        };
-    });
-    console.log(firstData);
+// Plot 1: Scatterplot of Psychic type Attack vs Special Attack
+firstData = rawData.map(d => {
+    return {
+        "Sp_Atk": +d.Sp_Atk,
+        "Attack": +d.Attack,
+        "Type_1": d.Type_1
+    };
+});
 
-    // Plot the scatterplot
-    const svg = d3.select("svg");
+// Plot the scatterplot
+const svg = d3.select("svg");
 
-    const g1 = svg.append("g")
-        .attr("width", scatterWidth + scatterMargin.left + scatterMargin.right)
-        .attr("height", scatterHeight + scatterMargin.top + scatterMargin.bottom)
-        .attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top + 30})`);
+const g1 = svg.append("g")
+    .attr("width", scatterWidth + scatterMargin.left + scatterMargin.right)
+    .attr("height", scatterHeight + scatterMargin.top + scatterMargin.bottom)
+    .attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top + 30})`);
 
-    // X label
-    g1.append("text")
-        .attr("x", scatterWidth / 2)
-        .attr("y", scatterHeight + 50)
-        .attr("font-size", "20px")
-        .attr("text-anchor", "middle")
-        .text("Attack");
+// Define the zoom function
+const zoom = d3.zoom()
+    .scaleExtent([0.5, 10]) // Set the scale extent for zooming
+    .on("zoom", zoomed);
 
-    // Y label
-    g1.append("text")
-        .attr("x", -(scatterHeight / 2))
-        .attr("y", -40)
-        .attr("font-size", "20px")
-        .attr("text-anchor", "middle")
-        .attr("transform", "rotate(-90)")
-        .text("Special Attack");
+// Apply the zoom behavior to the SVG element
+svg.call(zoom);
 
-    // Title
-    g1.append("text")
-        .attr("x", scatterWidth / 2)
-        .attr("y", -10)
-        .attr("font-size", "20px")
-        .attr("text-anchor", "middle")
-        .text("Special Attack vs Attack (hover to focus)");
+// Function to handle zooming and panning
+function zoomed() {
+    // Get the current transform of the zoom behavior
+    const transform = d3.event.transform;
+    
+    // Update the x and y scales based on the current transform
+    const newX = transform.rescaleX(x1);
+    const newY = transform.rescaleY(y1);
+    
+    // Update the axes with the new scales
+    g1.select(".x-axis").call(xAxisCall.scale(newX));
+    g1.select(".y-axis").call(yAxisCall.scale(newY));
+    
+    // Update the circles with the new scales
+    g1.selectAll(".data-circle")
+        .attr("cx", d => newX(d.Attack))
+        .attr("cy", d => newY(d.Sp_Atk));
+}
 
-    // X ticks
-    const x1 = d3.scaleLinear()
-        .domain([0, d3.max(firstData, d => d.Attack)])
-        .range([0, scatterWidth]);
+// X label
+g1.append("text")
+    .attr("x", scatterWidth / 2)
+    .attr("y", scatterHeight + 50)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Attack");
 
-    // Y ticks
-    const y1 = d3.scaleLinear()
-        .domain([0, d3.max(firstData, d => d.Sp_Atk)])
-        .range([scatterHeight, 0]);
+// Y label
+g1.append("text")
+    .attr("x", -(scatterHeight / 2))
+    .attr("y", -40)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Special Attack");
 
-    const xAxisCall = d3.axisBottom(x1)
-        .ticks(7);
+// Title
+g1.append("text")
+    .attr("x", scatterWidth / 2)
+    .attr("y", -10)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Special Attack vs Attack (hover to focus)");
 
-    g1.append("g")
-        .attr("transform", `translate(0, ${scatterHeight})`)
-        .call(xAxisCall)
-        .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "-5")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
+// X ticks
+const x1 = d3.scaleLinear()
+    .domain([0, d3.max(firstData, d => d.Attack)])
+    .range([0, scatterWidth]);
 
-    const yAxisCall = d3.axisLeft(y1)
-        .ticks(13);
-    g1.append("g").call(yAxisCall);
+// Y ticks
+const y1 = d3.scaleLinear()
+    .domain([0, d3.max(firstData, d => d.Sp_Atk)])
+    .range([scatterHeight, 0]);
 
-    const rects = g1.selectAll("circle").data(firstData);
+const xAxisCall = d3.axisBottom(x1)
+    .ticks(7);
 
-    // Create legend
-    const legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("transform", `translate(${scatterMargin.left + scatterWidth + 30}, ${scatterMargin.top + 30})`);
+g1.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${scatterHeight})`)
+    .call(xAxisCall)
+    .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
 
+const yAxisCall = d3.axisLeft(y1)
+    .ticks(13);
+g1.append("g")
+    .attr("class", "y-axis")
+    .call(yAxisCall);
 
-    const legendRectSize = 18;
-    const legendSpacing = 4;
+const rects = g1.selectAll("circle").data(firstData);
 
-    const legendRects = legend.selectAll(".legend-rect")
-        .data(Object.keys(typeToColor))
-        .enter().append("rect")
-        .attr("class", "legend-rect")
-        .attr("x", 0)
-        .attr("y", (d, i) => i * (legendRectSize + legendSpacing))
-        .attr("width", legendRectSize)
-        .attr("height", legendRectSize)
-        .style("fill", d => typeToColor[d])
-        .on("mouseout", resetOpacity)
-        .on("mouseover", highlightLegendColor)
+// Create legend
+const legend = svg.append("g")
+    .attr("class", "legend")
+    .attr("transform", `translate(${scatterMargin.left + scatterWidth + 30}, ${scatterMargin.top + 30})`);
 
+const legendRectSize = 18;
+const legendSpacing = 4;
+
+const legendRects = legend.selectAll(".legend-rect")
+    .data(Object.keys(typeToColor))
+    .enter().append("rect")
+    .attr("class", "legend-rect")
+    .attr("x", 0)
+    .attr("y", (d, i) => i * (legendRectSize + legendSpacing))
+    .attr("width", legendRectSize)
+    .attr("height", legendRectSize)
+    .style("fill", d => typeToColor[d])
+    .on("mouseout", resetOpacity)
+    .on("mouseover", highlightLegendColor)
 
 function highlightLegendColor(d) {
     const type = d;
-    g1.selectAll("circle")
+    g1.selectAll(".data-circle")
         .transition()
         .style("opacity", circle => circle.Type_1 === type ? opacityHigh : opacityLow);
 }
-    const legendTexts = legend.selectAll(".legend-text")
-        .data(Object.keys(typeToColor))
-        .enter().append("text")
-        .attr("class", "legend-text")
-        .attr("x", legendRectSize + legendSpacing)
-        .attr("y", (d, i) => i * (legendRectSize + legendSpacing) + (legendRectSize / 2))
-        .attr("dy", "0.35em")
-        .text(d => d);
+const legendTexts = legend.selectAll(".legend-text")
+    .data(Object.keys(typeToColor))
+    .enter().append("text")
+    .attr("class", "legend-text")
+    .attr("x", legendRectSize + legendSpacing)
+    .attr("y", (d, i) => i * (legendRectSize + legendSpacing) + (legendRectSize / 2))
+    .attr("dy", "0.35em")
+    .text(d => d);
 
-    function resetOpacity() {
-        g1.selectAll("circle")
-            .transition()
-            .style("opacity", opacityHigh);
-    }
+function resetOpacity() {
+    g1.selectAll(".data-circle")
+        .transition()
+        .style("opacity", opacityHigh);
+}
 
-    rects.enter().append("circle")
-        .attr("cx", d => x1(d.Attack))
-        .attr("cy", d => y1(d.Sp_Atk))
-        .attr("r", 3)
-        .attr("fill", d => typeToColor[d.Type_1])
-        .on("mouseover", highlightCircle)
-        .on("mouseout", resetOpacity);
+rects.enter().append("circle")
+    .attr("cx", d => x1(d.Attack))
+    .attr("cy", d => y1(d.Sp_Atk))
+    .attr("r", 3)
+    .attr("fill", d => typeToColor[d.Type_1])
+    .attr("class", "data-circle")
+    .on("mouseover", highlightCircle)
+    .on("mouseout", resetOpacity);
 
-    function highlightCircle(d) {
-        g1.selectAll("circle")
-            .transition()
-            .style("opacity", circle => circle.Type_1 === d.Type_1 ? opacityHigh : opacityLow);
-    }
+function highlightCircle(d) {
+    g1.selectAll(".data-circle")
+        .transition()
+        .style("opacity", circle => circle.Type_1 === d.Type_1 ? opacityHigh : opacityLow);
+}
+
 
     secondData = rawData.map(d => {
         return {
